1f406527fd2a835a03831ce54522bacf5f19f8cf
nimotsang <5320835@qq.com>
2016-11-17 18:49:52 +0800

    Remove bak files

--- js/common/menu.bak.js
+++
@@ -1,5 +0,0 @@
-﻿// Menu file loading
-var post = document.querySelector("link[rel = 'import']").import;
-var con = post.querySelector("#navi");
-var node = document.querySelector("#page-wrapper");
-document.querySelector("#wrapper").insertBefore(con, node);
